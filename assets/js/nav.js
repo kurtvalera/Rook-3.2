2c8869bc0e50d75c79bd851efcd5612d12ee980a
Kurt <kurt@rookdigitalunlimited.com>
2023-02-05 06:10:16 -0800

    overflow

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -33,9 +33,11 @@ document.addEventListener("DOMContentLoaded", function(){
   /* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
+  document.body.style.overflow = "hidden";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+  document.body.style.overflow = "visible";
 }
