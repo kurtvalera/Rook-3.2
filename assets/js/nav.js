defed808ce0db1823027ea0f5afe78c86b943a4f
Kurt <kurt@rookdigitalunlimited.com>
2023-02-05 06:08:28 -0800

    Sunday changes

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -29,3 +29,13 @@ document.addEventListener("DOMContentLoaded", function(){
   
   }); 
   
+
+  /* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
--- index.php
+++ index.php
@@ -25,7 +25,9 @@
 		<link rel="stylesheet" href="assets/css/s7.css" />
 		<link rel="stylesheet" href="assets/css/s8.css" />
 		<link rel="stylesheet" href="assets/css/s9.css" />
+		<link rel="stylesheet" href="assets/css/navmenu.css" />
 
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<!-- Owl -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -50,6 +52,7 @@
 
 	<?php
 		include ('html/section_1.html');
+		include ('html/section_navmenu.html');
 		include ('html/section_2.html');
 		include ('html/section_3.html');
 		include ('html/section_4.html');
